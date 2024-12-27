6671162a6db530cb469fcd63012b4a1b2c97c792
satorutoji <ezdummyacc@gmail.com>
2024-12-27 22:56:13 +0300

    1.0.0 typescritp

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import {sum} from "./chlenohuilka"
+
+console.log('privet')
+let a = 5
+let b = 6
+let c= a + b
+console.log(c)
+console.log(sum(6,63))
---
+++ webpack.config.ts
@@ -0,0 +1,31 @@
+import path from 'path'
+import webpack from 'webpack'
+import HTMLWebpackPlugin from 'html-webpack-plugin'
+
+export default (env: any) => {
+  const config: webpack.Configuration = {
+    mode: env.mode ?? 'development',
+    entry: path.resolve(__dirname, 'src', 'index.ts'),
+    output: {
+      path: path.resolve(__dirname, 'build'),
+      filename: '[name].[contenthash].js',
+      clean: true
+    },
+    plugins: [
+      new HTMLWebpackPlugin({ template: path.resolve(__dirname, 'public', 'index.html')})
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    }  
+  }
+  return config
+} 
